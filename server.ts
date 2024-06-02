0dc4d08d0e57f97dd33f73726d809246f22263ad
SureshYarlanki <143301284+SureshYarlanki@users.noreply.github.com>
2024-06-02 12:46:17 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -19,9 +19,13 @@ app.use(express.json())
 dotenv.config({
     path:'./.env'
 })
-const port:number|string = process.env.PORT||9999
-const dbUrl:string | undefined =process.env.MONGO_DB_CLOUD_URL  
-const dbName:string | undefined =process.env.MONGO_DB_DATABASE  
+const port:number|string = process.env.PORT||10000
+const dbUrl:string | undefined ="mongodb+srv://sureshyarlanki83:SURESH%40123@react-mern.tbushkw.mongodb.net/?retryWrites=true&w=majority&appName=REACT-MERN "
+const dbName:string | undefined ="contact-manager"
+
+// const port:number|string = process.env.PORT||9999
+// const dbUrl:string | undefined =process.env.MONGO_DB_CLOUD_URL  
+// const dbName:string | undefined =process.env.MONGO_DB_DATABASE  
 
 app.get("/", (request:Request, response:Response )=> {
     response.status(200);
@@ -37,18 +41,33 @@ app.use("/groups", groupRouter)
 
 if (port) {
     app.listen(port,  () => {
-
-            (async () => {
+          (async () => {
                 try {
                     if (dbUrl && dbName) {
-                        const result = await DBUtil.connectToDB(dbUrl, dbName);
-                    console.log(result); // Output: Connected to MongoDB successfully
+                        DBUtil.connectToDB(dbUrl, dbName).then((dbResponse) => {
+                            console.log(dbResponse)
+                        }).catch((error) => {
+                            console.log(error)
+                            process.exit(0)//force stop express server
+                        })
                     }
                     
                 } catch (error) {
                     console.error(error); // Output: MongoDB connection failed: [Detailed Error Message]
                 }
             })();
+
+            // (async () => {
+            //     try {
+            //         if (dbUrl && dbName) {
+            //             const result = await DBUtil.connectToDB(dbUrl, dbName);
+            //         console.log(result); // Output: Connected to MongoDB successfully
+            //         }
+                    
+            //     } catch (error) {
+            //         console.error(error); // Output: MongoDB connection failed: [Detailed Error Message]
+            //     }
+            // })();
         console.log(`Express server is started at http://${port}`)
        
     })
